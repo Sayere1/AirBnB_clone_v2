cbe225d469a40ed5f1aa73dee97d6cc40d32f4f1
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2024-02-21 20:20:42 +0200

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,31 +12,51 @@ time_fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 if getenv("HBNB_TYPE_STORAGE") == 'db':
     Base = declarative_base()
-else:
-    Base = object
+# else:
+#     Base = object
 
 
 class BaseModel:
     """A Base class for all hbnb models"""
 
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-        updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    # if getenv("HBNB_TYPE_STORAGE") == 'db':
+    #     id = Column(String(60), nullable=False, primary_key=True)
+    #     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    #     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
+    # def __init__(self, *args, **kwargs):
+    #     """base model initialized"""
+    #     self.id = str(uuid.uuid4())
+    #     self.created_at = datetime.now()
+    #     self.updated_at = self.created_at
+    #     for key, value in kwargs.items():
+    #         if key == '__class__':
+    #             continue
+    #         setattr(self, key, value)
+    #         if type(self.created_at) is str:
+    #             self.created_at = datetime.strptime(self.created_at, time_fmt)
+    #         if type(self.updated_at) is str:
+    #             self.updated_at = datetime.strptime(self.updated_at, time_fmt)
     def __init__(self, *args, **kwargs):
-        """base model initialized"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        for key, value in kwargs.items():
-            if key == '__class__':
-                continue
-            setattr(self, key, value)
-            if type(self.created_at) is str:
-                self.created_at = datetime.strptime(self.created_at, time_fmt)
-            if type(self.updated_at) is str:
-                self.updated_at = datetime.strptime(self.updated_at, time_fmt)
+        """Instatntiates a new model"""
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                        '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            for i, j in kwargs.items():
+                if i not in ['id', 'created_at', 'updated_at']:
+                    self.__setattr__(i, j)
+            self.__dict__.update(kwargs)
+
 
     def __str__(self):
         """return String representation of the BaseModel class"""
