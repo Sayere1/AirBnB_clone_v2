52e087a21b319f51d947a0603ba3a5bcd5896744
Sayere1 <osayemwenreosaigbovo@gmail.com>
2024-03-24 09:18:42 -0700

    Py cool, number, temp, odd/even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application.
+App listens on 0.0.0.0, port 5000. Routes:
+    /: Return 'Hello HBNB!'. /c/<text>: Return 'C' then <text>.
+    /hbnb: Return 'HBNB'. /python/(<text>): Return 'Python' then <text>.
+    /number/<n>: Return 'n is a num' only if <n> is an int.
+    /number_template/<n>: Return an HTML page only if <n> is an integer.
+        - Displays the value of <n> in the body.
+    /number_odd_or_even/<n>: Return an HTML page only if <n> is an integer.
+        - (n) could be odd or even and must be stated in the body.
+"""
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Return 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Return 'HBNB'"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Return 'C' then <text>"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Return 'Python' then <text>"""
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """Return 'n is a num' only if <n> is an int."""
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """ Return an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    """Return an HTML page only if <n> is an integer.
+
+    (n) could be odd or even and must be stated in the body.
+    """
+    return render_template("6-number_odd_or_even.html", n=n)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
